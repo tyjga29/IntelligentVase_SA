d3d1b8b9e26ca8293eb67f9689291023fb9aa5a9
Tobias Kapp <kapp.tob@gmail.com>
2024-01-23 15:23:41 +0100

    Comparison table

--- Main_Controller/logic/plants_config/get_plant_attributes.py
+++ Main_Controller/logic/plants_config/get_plant_attributes.py
@@ -2,12 +2,25 @@ import yaml
 
 attributes_file = "Main_Controller/logic/plants_config/plants_attributes.yaml"
 
-def get_plant_attributes():
-    # Read the YAML file with plant attributes
+def get_plants_columns_mapping():
     try:
         with open(attributes_file, 'r') as yaml_file:
             plant_attributes = yaml.safe_load(yaml_file)
             return plant_attributes
+    except Exception as e:
+        # Handle exceptions, print an error message, or return an empty list as needed
+        print(f"Error: {e}")
+        return []
+    
+def get_plant_attributes():
+    try:
+        with open(attributes_file, 'r') as file:
+            data = yaml.safe_load(file)
+
+        # Extracting values only
+        values = list(data.values())
+
+        return values
     except Exception as e:
         # Handle exceptions, print an error message, or return an empty list as needed
         print(f"Error: {e}")
